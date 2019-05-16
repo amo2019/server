2cd07ad54f5cda555c20a7ff0e05e9aea25f2a97
Amo2019 <aminvpython@gmail.com>
2019-05-16 22:03:22 +0200

    New Changes

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require ('express');
 const app = express();
 
 app.get('/', (req, res)=> {
-  res.send({Hi: 'There'});
+  res.send({Hi: 'Hi There, Say Hello'});
 });
 const PORT = process.env.PORT || 5000;
 //app.listen(PORT);
