44ba789564d6062b4c8a07a6740168d2038a068c
Amo2019 <aminvpython@gmail.com>
2019-05-16 20:15:49 +0200

    initial commit

---
+++ index.js
@@ -0,0 +1,13 @@
+//common js modules, ES2015 modules import express from 'express';
+const express = require ('express');
+const app = express();
+
+app.get('/', (req, res)=> {
+  res.send({Hi: 'There'});
+});
+const PORT = process.env.PORT || 5000;
+//app.listen(PORT);
+
+app.listen(PORT, function () {
+  console.log('Example app listening on port ' + PORT + '!');
+});
