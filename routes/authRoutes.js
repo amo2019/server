654360134bc8722b3c001130e58d508b46998bf9
Amo2019 <aminvpython@gmail.com>
2019-05-22 20:04:20 +0200

    finished Auth flow

---
+++ routes/authRoutes.js
@@ -0,0 +1,30 @@
+const passport = require('passport');
+
+module.exports = app => {
+      app.get('/auth/google', passport.authenticate('google', {
+        scope: ['profile', 'email']
+      })
+      );
+      // app.get('/', (req, res)=> {
+      //   res.send({Hi: 'Hi There, Say Hello'});
+      // });
+      app.get('/auth/google/callback', passport.authenticate('google'));
+
+      app.get('/api/logout', (req, res) => {
+        //res.send('U R Loged out!');
+        req.logout();
+        res.send(req.user);
+        //JSON.stringify(req.user);
+        //JSON.parse(req.user);
+        //res.send(JSON.stringify(user));
+        //res.send('U R Loged out!');
+  //       res.statusCode = 200;
+  // res.setHeader('Content-Type', 'text/plain');
+  // res.end('U R Loged out!\n');
+      });
+
+      app.get('/api/current-user', (req, res) => {
+      //res.send(req.session);
+       res.send(req.user);
+      });
+};
---
+++ services/passport.js
@@ -0,0 +1,47 @@
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const keys = require('../config/keys');
+const mongoose = require('mongoose');
+
+const User = mongoose.model('users');
+
+passport.serializeUser((user, done) => {
+  done(null, user.id);
+});
+
+passport.deserializeUser((id, done) => {
+  User.findById(id).then(user => {
+    done(null, user);
+  });
+});
+
+passport.use(new GoogleStrategy({
+clientID: keys.googleClientID,
+clientSecret: keys.googleClientSecret,
+callbackURL: '/auth/google/callback'
+},
+(accessToken, refreshToken, profile, done) => {
+//   console.log('access token: ', accessToken);
+//   console.log('refresh token: ', refreshToken);
+
+User.findOne({ googleID: profile.id })
+.then(existingUser => {
+  if (existingUser) {
+    //console.log('Already Exist: ', profile.id);
+    done(null, existingUser);
+
+
+  } else {
+    new User({ googleID: profile.id })
+    .save()
+    .then(user => done(null, user))
+    .catch(error => cb(error, null));
+      //  console.log(error);
+    //console.log('profile!: ', profile.id);
+
+  }
+});
+
+}
+)
+);
