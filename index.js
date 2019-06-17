67b009d4e241b969121e28b7413af69333420a24
Amo2019 <aminvpython@gmail.com>
2019-06-17 21:43:15 +0200

    push production new App35

--- index.js
+++ index.js
@@ -34,7 +34,7 @@ if (process.env.MODE_ENV === 'production') {
   // Express willserve up the index.html
   // if it doesn't recognize the routes
   const path = require('path');
-  app.get('/', (req, res) => {
+  app.get('*', (req, res) => {
     res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
   });
 }
