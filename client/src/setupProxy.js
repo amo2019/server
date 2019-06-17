66d6bf2803daa32d5a49f0e940d642199cb71146
Amo2019 <aminvpython@gmail.com>
2019-06-17 21:28:39 +0200

    push production new App35

--- client/src/setupProxy.js
+++ client/src/setupProxy.js
@@ -4,5 +4,4 @@ module.exports = function(app) {
   app.use(proxy('/auth/google', { target: 'http://localhost:5000' }))
     app.use(proxy('/api', { target: 'http://localhost:5000'}))
     app.use(proxy('/api/*', { target: 'http://localhost:5000'}))
-    app.use(proxy('*', { target: 'http://localhost:5000'}))
 }
