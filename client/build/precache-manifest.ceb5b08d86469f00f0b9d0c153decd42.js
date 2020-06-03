ed1f32942edfe4684beb328874b251d85be294f9
amo2019 <aminvpython@gmail.com>
2020-06-04 00:03:02 +0200

    editing SurveyList.js

---
+++ client/build/precache-manifest.ceb5b08d86469f00f0b9d0c153decd42.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e142655c6a39edce7d1e011ac8896d58",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a499cc2b681b0c63f3f4",
+    "url": "/static/css/2.36020323.chunk.css"
+  },
+  {
+    "revision": "a499cc2b681b0c63f3f4",
+    "url": "/static/js/2.4158deec.chunk.js"
+  },
+  {
+    "revision": "64bea2e190f27355a76963b8cb94bb7b",
+    "url": "/static/js/2.4158deec.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e57e311262a07b922143",
+    "url": "/static/js/main.12c03bbd.chunk.js"
+  },
+  {
+    "revision": "13c5ce7c89dba05c4fba",
+    "url": "/static/js/runtime-main.8c40394c.js"
+  }
+]);
--- client/build/service-worker.js
+++ client/build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.8a209fabed917ee239826e1294d60c5e.js"
+  "/precache-manifest.ceb5b08d86469f00f0b9d0c153decd42.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -35,5 +35,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
