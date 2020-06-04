0096d6eeb743d4612b395e56efeca7a5efed726e
amo2019 <aminvpython@gmail.com>
2020-06-04 06:34:32 +0200

    yarn update

---
+++ client/build/precache-manifest.e66c5e58fd3ded53b774bd24b8fa22bd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "066a6481aa3c2c616e48945a394e848e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5238152643b3a2937e8f",
+    "url": "/static/css/2.36020323.chunk.css"
+  },
+  {
+    "revision": "5238152643b3a2937e8f",
+    "url": "/static/js/2.6fc110bb.chunk.js"
+  },
+  {
+    "revision": "64bea2e190f27355a76963b8cb94bb7b",
+    "url": "/static/js/2.6fc110bb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "004adc5e65ba6366e3f2",
+    "url": "/static/js/main.84ba9cd9.chunk.js"
+  },
+  {
+    "revision": "13c5ce7c89dba05c4fba",
+    "url": "/static/js/runtime-main.8c40394c.js"
+  }
+]);
